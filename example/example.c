47855ef8b1123ed10e63fa6f0f459e5641f67165
bryan newbold <bnewbold@twinleaf.com>
2014-06-25 14:57:57 -0400

    example code: add floating point stuff

--- example/example.c
+++ example/example.c
@@ -18,15 +18,23 @@ int main() {
     modbus_set_slave(mb, 2);
     modbus_connect(mb);
 
+    // http://libmodbus.org/docs/v3.1.1/modbus_read_registers.html
     rc = modbus_read_registers(mb, 0, 20, tab_reg);
     if (rc == -1) {
         fprintf(stderr, "%s\n", modbus_strerror(errno));
         return -1;
     }
 
+    printf("======= Raw Register Dump");
     for (i=0; i < rc; i++) {
         printf("reg[%d]=%d (0x%X)\n", i, tab_reg[i], tab_reg[i]);
     }
+
+    printf("======= Floating Point Values");
+    // http://libmodbus.org/docs/v3.1.1/modbus_get_float.html
+    printf("Float Mag X: %f\n", modbus_get_float(&tab_reg[2]));
+    printf("Float Mag Y: %f\n", modbus_get_float(&tab_reg[4]));
+    printf("Float Mag Z: %f\n", modbus_get_float(&tab_reg[6]));
 
     modbus_close(mb);
     modbus_free(mb);
